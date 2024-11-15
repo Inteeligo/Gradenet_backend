cd9c6b021a2e73c6dcfcd11ae3d688b46a09c2a5
olukinni_029 <olukinnif`150216029@gmail.com>
2024-11-15 16:44:48 +0100

    Add an endpoint

--- server.js
+++ server.js
@@ -65,6 +65,10 @@ app.post("/api/contact", (req, res) => {
   });
 });
 
+app.get('/',(req,res)=>{
+  res.send('Hello World!')
+});
+
 // Synchronize the database and start the server once successful
 syncDatabase()
   .then(() => {
